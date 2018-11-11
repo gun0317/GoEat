1cc3d51260d614e2e3bfb72202cff93455b841a6
taenyun93 <35717288+taenyun93@users.noreply.github.com>
2018-11-11 19:31:23 +0900

    181111_2

--- GoEat.py
+++ GoEat.py
@@ -453,7 +453,49 @@ class user_profiles_builder:
         return item_profile
 
 
+class HybridRecommender:
 
+    MODEL_NAME = 'Hybrid'
+
+    def __init__(self, cb_rec_model, cf_rec_model, items_df):
+        self.cb_rec_model = cb_rec_model
+        self.cf_rec_model = cf_rec_model
+        self.items_df = items_df
+
+    def get_model_name(self):
+        return self.MODEL_NAME
+
+    def recommend_items(self, user_id, items_to_ignore=[], topn=10, verbose=False):
+        #Getting the top-1000 Content-based filtering recommendations
+        cb_recs_df = self.cb_rec_model.recommend_items(user_id, items_to_ignore=items_to_ignore, verbose=verbose,
+                                                           topn=1000).rename(columns={'recStrength': 'recStrengthCB'})
+
+        #Getting the top-1000 Collaborative filtering recommendations
+        cf_recs_df = self.cf_rec_model.recommend_items(user_id, items_to_ignore=items_to_ignore, verbose=verbose,
+                                                           topn=1000).rename(columns={'recStrength': 'recStrengthCF'})
+
+        #Combining the results by foodId
+        recs_df = cb_recs_df.merge(cf_recs_df,
+                                   how = 'inner',
+                                   left_on = 'foodId',
+                                   right_on = 'foodId')
+
+        #Computing a hybrid recommendation score based on CF and CB scores
+        recs_df['recStrengthHybrid'] = recs_df['recStrengthCB'] * recs_df['recStrengthCF']
+
+        #Sorting recommendations by hybrid score
+        recommendations_df = recs_df.sort_values('recStrengthHybrid', ascending=False).head(topn)
+
+        if verbose:
+            if self.items_df is None:
+                raise Exception('"items_df" is required in verbose mode')
+
+            recommendations_df = recommendations_df.merge(self.items_df, how = 'left',
+                                                          left_on = 'foodId',
+                                                          right_on = 'foodId')[['recStrengthHybrid', 'foodId', 'foodName']]
+
+
+        return recommendations_df
 
 
 def joint_recommender_model(recommender_model, *args):
