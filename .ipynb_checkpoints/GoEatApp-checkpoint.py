65eafda72ef92f3a540c228c099cfa8ca91ebaed
taenyun93 <35717288+taenyun93@users.noreply.github.com>
2018-11-25 17:17:30 +0900

    Jupyter Update

--- .ipynb_checkpoints/GoEatApp-checkpoint.py
+++ .ipynb_checkpoints/GoEatApp-checkpoint.py
@@ -12,13 +12,39 @@ import sklearn
 import konlpy
 import re
 
-#import dataframes
-df_user_info = pd.read_csv('data\\info\\df_user_info.csv')
-df_user_detail =pd.read_csv('data\\info\\df_user_detail.csv')
-food_df = pd.read_csv('data\\info\\food_df.csv')
-food_detail_df = pd.read_csv('data\\info\\food_detail_df.csv')
-interactions_df = pd.read_csv('data\\info\\interactions_df.csv')
+db = pymysql.connect(host = 'DESKTOP-PD3BJSG',port=3306,user='mysql',password ='mysql93',db='goeat',charset = 'utf8')
 
+cursor = db.cursor()
+
+sql ="SELECT table_name FROM information_schema.tables where table_schema='goeat'"
+cursor.execute(sql)
+table = cursor.fetchall()
+
+table_names = []
+for name in table:
+    table_names.append(name[0])
+    
+for table_name in table_names:
+    table_select = "SELECT * FROM " + table_name
+    col_show = col = "SHOW COLUMNS FROM " + table_name
+    
+    cursor.execute(table_select)
+    data = cursor.fetchall()
+
+    cursor.execute(col_show)
+    columns = cursor.fetchall()
+    
+    col_name = []
+    for column in columns:
+        col_name.append(column[0].replace('\ufeff',''))
+    
+    vars()[table_name +'_df'] = pd.DataFrame(list(data),columns = col_name)
+    vars()[table_name +'_df'].index = vars()[table_name +'_df'].index + 1
+
+tfidf_df = tfidf_df.drop('foodIndex',axis=1)
+
+tfidf_feature_names = tfidf_df.columns
+tfidf_matrix = scipy.sparse.csr_matrix(tfidf_df)
 
 #get new user info
 
@@ -28,9 +54,6 @@ new_user = GoEat.cold_start_question(999,food_df,15)
 
 interactions_df = interactions_df.append(new_user,ignore_index=True)
 ##Content-based filtering##
-#import tfidf_matrix
-with open('tfidf.pickle', 'rb') as f:
-    tfidf_matrix, tfidf_feature_names = pickle.load(f)
 
 #build user profiles
 builder= GoEat.user_profiles_builder()
@@ -54,7 +77,9 @@ cf_model = GoEat.CFRecommender(cf_preds_df, food_df)
 
 #Hybrid Model buliding
 hybrid_model = GoEat.HybridRecommender(cb_model,cf_model,food_df,method='harmonic')
-
-print('당신이 좋아하는 음식은!')
-print(hybrid_model.recommend_items(999,topn=3,items_to_ignore=new_user.foodId.tolist(),verbose=True).foodName)
-
+rec_foods = hybrid_model.recommend_items(999,topn=3,items_to_ignore=new_user.foodId.tolist(),verbose=True)
+print('\n')
+print('당신께 추천드리는 음식은!!!\n')
+print('1번 째!  ' + rec_foods.loc[0,'foodName'])
+print('2번 째!  ' + rec_foods.loc[1,'foodName'])
+print('3번 째!  ' + rec_foods.loc[2,'foodName'])
